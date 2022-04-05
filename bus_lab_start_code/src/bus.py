1c0b3f262103bf78d7f2616ce031480bf29f60b7
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-04-05 13:49:43 +0100

    all done

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -4,3 +4,22 @@ class Bus:
         self.destination = destination
         self.passengers = []
 
+    def drive(self):
+        return "Brum brum"
+
+    def passenger_count(self):
+        return len(self.passengers)
+
+    def pick_up(self, person):
+        self.passengers.append(person)
+
+    def drop_off(self, person):
+        self.passengers.remove(person)
+        return self.passenger_count()
+
+    def empty(self):
+        self.passengers.clear()
+
+    def pick_up_from_stop(self, bus_stop):
+        self.passengers.extend(bus_stop.queue)
+        bus_stop.queue.clear()
